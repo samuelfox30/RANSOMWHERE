a8dc7a005ce8d4ee9afedeb4395e9b16f0e583d8
conversaox <samuel@conversaox.com.br>
2025-09-10 20:47:43 -0300

    detesto gay

--- core/Detection.cs
+++ core/Detection.cs
@@ -1,39 +1,44 @@
 ﻿using System;
-using System.Collections.Concurrent;
 using System.IO;
-using System.Threading;
+using System.Threading.Tasks;
 
 namespace Monitoramento
 {
     public class Detection
     {
-
-        public void Detector(string fileWay){
+        public void Detector(string fileWay)
+        {
             Console.WriteLine($"Verificando arquivo {fileWay}...");
         }
 
-        public async Task<bool> stability(string caminho, string delay = 100){
-            if (!File.Exists(caminho)){
+        public async Task<bool> Stability(string caminho, int delay = 100)
+        {
+            if (!File.Exists(caminho))
+            {
                 return false;
             }
 
-            while (true){
-                await (Task.Delay(delay)
-                try{
+            while (true)
+            {
+                await Task.Delay(delay);
+
+                try
+                {
                     using var fs = new FileStream(caminho, FileMode.Open, FileAccess.Read, FileShare.None);
                     long tamanho1 = fs.Length;
-                    await Task.Delay(50)
+                    await Task.Delay(50);
                     long tamanho2 = fs.Length;
-                    if tamanho1 == tamanho2{
+
+                    if (tamanho1 == tamanho2)
+                    {
                         return true;
                     }
                 }
-                catch (IOException){
-                    // deu erro → arquivo ainda em uso (instável)
+                catch (IOException)
+                {
+                    // Arquivo ainda em uso (instável)
                 }
             }
-            
         }
-
     }
 }
--- obj/Debug/net9.0/Monitoramento.AssemblyInfo.cs
+++ obj/Debug/net9.0/Monitoramento.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Monitoramento")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60870361f932409d73cb149a4156a3adb991918e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78042b892a7b62a1fd2730686e09161f31dbfc63")]
 [assembly: System.Reflection.AssemblyProductAttribute("Monitoramento")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Monitoramento")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
